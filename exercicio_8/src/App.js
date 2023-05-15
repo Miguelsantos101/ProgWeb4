95163fa395a8500e6c0e2c6310b858128fc59dcd
Miguel dos Santos Flores <miiguelsantos05@hotmail.com>
2023-05-15 19:18:04 -0400

    Alterações nas pastas e começando Exercício 8

---
+++ exercicio_8/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+          <h1>PLANO DE ENSINO</h1>
+      </header>
+
+        
+
+    </div>
+  );
+}
+
+export default App;
