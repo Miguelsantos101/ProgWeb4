e89e57ade1108949ffc7309407d9a52f85c5ad49
miguel.flores <miiguelsantos05@hotmail.com>
2023-05-22 16:45:47 -0400

    Exercício 10

---
+++ exercicio_10/src/pages/NaoEncontrado/index.js
@@ -0,0 +1,9 @@
+function NaoEncontrado() {
+  return (
+    <div>
+      <h1>Página não encontrada</h1> <br /> <br />
+    </div>
+  );
+}
+
+export default NaoEncontrado;
---
+++ exercicio_10/src/routes.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
+import Contato from "./pages/Contato";
+import NaoEncontrado from "./pages/NaoEncontrado";
+
+import Header from "./components/Header";
+
+function RoutesApp() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/contato" element={<Contato />} />
+        <Route path="*" element={<NaoEncontrado />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default RoutesApp;
