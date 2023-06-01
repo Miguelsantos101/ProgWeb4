570153dd3a01092bf7d3febbc61443482f67d52d
miguel.flores <miiguelsantos05@hotmail.com>
2023-06-01 10:25:44 -0400

    Exercício 11

---
+++ exercicio_11/src/pages/Film/index.js
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import api from "../../services/api";
+import "./filme.css";
+
+function Film() {
+  const { id } = useParams();
+  const [film, setFilm] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadFilm() {
+      await api
+        .get(`/movie/${id}`, {
+          params: {
+            api_key: "f6a8672f5d18fe891e31254590f7f36e",
+            language: "pt-BR",
+          },
+        })
+        .then((response) => {
+          setFilm(response.data);
+          setLoading(false);
+        })
+        .catch(() => {
+          console.log("Filme não encontrado");
+        });
+    }
+
+    loadFilm();
+
+    return () => {
+      console.log("Componente foi desmontado");
+    };
+  }, [id]);
+
+  if (loading) {
+    return (
+      <div className="filme-info">
+        <h1>Carregando detalhes..</h1>
+      </div>
+    );
+  }
+  return (
+    <div className="filme-info">
+      <h1>{film.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${film.backdrop_path}`}
+        alt={film.title}
+      />
+
+      <h3>Sinopse</h3>
+      <span>{film.overview}</span>
+      <strong>Avaliação: {film.vote_average} / 10</strong>
+    </div>
+  );
+}
+
+export default Film;
